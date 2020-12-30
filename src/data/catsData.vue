d08b64d495945046864a9d79117ddca6fae1a6ff
gitsu747 <surajsethurv@gmail.com>
2020-12-30 13:39:48 +0530

    Added a new cat
    Testing update to git.

--- src/data/catsData.vue
+++ src/data/catsData.vue
@@ -3,7 +3,8 @@ export default [
   { isActive: true, age: 40, firstName: 'Dickerson', type: 'Macdonald' },
   { isActive: false, age: 21, firstName: 'Larsen', type: 'Shaw' },
   { isActive: false, age: 89, firstName: 'Geneva', type: 'Wilson' },
-  { isActive: true, age: 38, firstName: 'Jami', type: 'Carney' }
+  { isActive: true, age: 38, firstName: 'Jami', type: 'Carney' },
+  { isActive: true, age: 8, firstName: 'Meow', type: 'Normal' }
 ]
 
 </script>

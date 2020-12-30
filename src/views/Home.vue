e92bba5b9cdcd324d00cf66d2532d58921ecce48
gitsu747 <surajsethurv@gmail.com>
2020-12-30 16:41:28 +0530

    SASS
    Tried adding sass but maybe the project initalization was wrong. Added styles with
    scss anyway and it worked. but the hierarchy was not achived. that colour comes
    from the components scss rather that the base. this will have to be fixed
    later on.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,55 +1,56 @@
 <template>
-  <div class="home">
-    Home Page
-    Total count of pets : {{getterPetCount}}
-    <br />
-    <button @click="toggleFormVisibility" class="btn btn-primary">ADD PET</button>
+  <div class="home-view-container">
+    <h1>Home Page</h1>
+      <br />
+  Total count of pets : {{getterPetCount}}
+  <br />
+  <button @click="toggleFormVisibility" class="btn btn-primary">ADD PET</button>
 
-    <div id="formDiv" v-if="showForm">
-      <b-form @submit.prevent="handleSubmit">
-        <b-form-group id="input-group-1"
-                      label="First Name"
-                      label-for="firstName"
-                      description="Please enter your first name">
-          <b-form-input id="input-1"
-                        v-model="form.firstName"
-                        type="text"
-                        placeholder="Enter Name"
-                        required></b-form-input>
-        </b-form-group>
+  <div id="formDiv" v-if="showForm">
+    <b-form @submit.prevent="handleSubmit">
+      <b-form-group id="input-group-1"
+                    label="First Name"
+                    label-for="firstName"
+                    description="Please enter your first name">
+        <b-form-input id="input-1"
+                      v-model="form.firstName"
+                      type="text"
+                      placeholder="Enter Name"
+                      required></b-form-input>
+      </b-form-group>
 
-        <b-form-group id="input-group-2" label="Pet Type:" label-for="input-2">
-          <b-form-input id="input-2"
-                        v-model="form.type"
-                        placeholder="Enter Breed"
-                        required></b-form-input>
-        </b-form-group>
+      <b-form-group id="input-group-2" label="Pet Type:" label-for="input-2">
+        <b-form-input id="input-2"
+                      v-model="form.type"
+                      placeholder="Enter Breed"
+                      required></b-form-input>
+      </b-form-group>
 
-        <b-form-group id="input-group-3" label="Age:" label-for="input-3">
-          <b-form-input id="input-3"
-                        v-model="form.age"
-                        type="number"
-                        placeholder="Enter age"
-                        required></b-form-input>
-        </b-form-group>
+      <b-form-group id="input-group-3" label="Age:" label-for="input-3">
+        <b-form-input id="input-3"
+                      v-model="form.age"
+                      type="number"
+                      placeholder="Enter age"
+                      required></b-form-input>
+      </b-form-group>
 
-        <b-form-group id="input-group-3" label="Species:" label-for="input-3">
-          <b-form-select id="input-3"
-                         v-model="form.species"
-                         :options="speciesData"
-                         placeholder="Choose a Species"
-                         required></b-form-select>
-        </b-form-group>
+      <b-form-group id="input-group-3" label="Species:" label-for="input-3">
+        <b-form-select id="input-3"
+                       v-model="form.species"
+                       :options="speciesData"
+                       placeholder="Choose a Species"
+                       required></b-form-select>
+      </b-form-group>
 
-        <b-button type="submit" variant="primary">Submit</b-button>
-        <!--<b-button type="reset" variant="danger">Reset</b-button>-->
-      </b-form>
-      <!--<b-card class="mt-3" header="Form Data Result">
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <!--<b-button type="reset" variant="danger">Reset</b-button>-->
+    </b-form>
+    <!--<b-card class="mt-3" header="Form Data Result">
       <pre class="m-0">{{ form }}</pre>
     </b-card>-->
-    </div>
-
   </div>
+
+</div>
 </template>
 
 <script>
